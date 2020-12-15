91a33c99fbc587e7b417fe48b8502a15455ed36c
taylor2230 <60805326+taylor2230@users.noreply.github.com>
2020-12-15 15:05:00 -0600

    updating files

--- PHP/goalParser.php
+++ PHP/goalParser.php
@@ -1,10 +1,12 @@
 class Solution {
 
-    /**
-    * @param String $command
-    * @return String
-    */
-    function interpret($command) {
-        
-    }
+/**
+* @param String $command
+* @return String
+*/
+function interpret($command) {
+$toReplace = array("()", "(al)");
+$replaced = array("o", "al");
+return str_replace($toReplace, $replaced, $command);
+}
 }
